e1c8f58628d1c82bdd7fc6a87990803afdaa82cf
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2023-03-04 20:31:31 -0500

    integrando scraffolding dbContext

---
+++ BlazorCrud.Server/Models/DbcrudBlazorContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Server.Models;
+
+public partial class DbcrudBlazorContext : DbContext
+{
+    public DbcrudBlazorContext()
+    {
+    }
+
+    public DbcrudBlazorContext(DbContextOptions<DbcrudBlazorContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Departamento> Departamentos { get; set; }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-UJ2B95O;Initial Catalog=DBCrudBlazor;Integrated Security=True; Trusted_Connection=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Departamento>(entity =>
+        {
+            entity.HasKey(e => e.IdDepartamento).HasName("PK__Departam__787A433DE1AA0E0F");
+
+            entity.ToTable("Departamento");
+
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("PK__Empleado__CE6D8B9ED2A90401");
+
+            entity.ToTable("Empleado");
+
+            entity.Property(e => e.FechaContrato).HasColumnType("date");
+            entity.Property(e => e.NombreCompleto)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdDepartamentoNavigation).WithMany(p => p.Empleados)
+                .HasForeignKey(d => d.IdDepartamento)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Empleado__IdDepa__38996AB5");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
