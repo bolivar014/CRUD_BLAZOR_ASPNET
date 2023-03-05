3d0ece37e17def08aafd5e0769d3f67086582c17
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2023-03-04 20:37:23 -0500

    ajustando cadena de conexión migrando desde AppSettings.JSON

--- BlazorCrud.Server/Models/DbcrudBlazorContext.cs
+++ BlazorCrud.Server/Models/DbcrudBlazorContext.cs
@@ -20,8 +20,9 @@ public partial class DbcrudBlazorContext : DbContext
     public virtual DbSet<Empleado> Empleados { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-UJ2B95O;Initial Catalog=DBCrudBlazor;Integrated Security=True; Trusted_Connection=True; TrustServerCertificate=True;");
+    {
+
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- BlazorCrud.Server/Program.cs
+++ BlazorCrud.Server/Program.cs
@@ -1,3 +1,6 @@
+using BlazorCrud.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Obtenemos la cadena de conexión desde el AppSettings.json
+builder.Services.AddDbContext<DbcrudBlazorContext>( opciones =>
+{
+    opciones.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"));
+});
 
 var app = builder.Build();
 
