7b51a66632687d6906b24d87f0d74a709ddf40dc
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2023-03-05 11:31:57 -0500

    actualizando a VS .net7.0 y reinstalando nuggets necesarios

---
+++ BlazorCrud.Server/Controllers/DepartamentoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// 
+using BlazorCrud.Server.Models;
+// using BlazorCrud.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentoController : ControllerBase
+    {
+    }
+}
