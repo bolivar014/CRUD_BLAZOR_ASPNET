d1da01e83a1bbf2f0cf1ce2545755b90729a41d3
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2023-03-05 11:40:20 -0500

    integrando endpoint para listar departamentos

--- BlazorCrud.Server/Controllers/DepartamentoController.cs
+++ BlazorCrud.Server/Controllers/DepartamentoController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 // 
 using BlazorCrud.Server.Models;
-// using BlazorCrud.Shared;
+using BlazorCrud.Shared;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazorCrud.Server.Controllers
@@ -12,5 +12,46 @@ namespace BlazorCrud.Server.Controllers
     [ApiController]
     public class DepartamentoController : ControllerBase
     {
+        private readonly DbcrudBlazorContext _dbContext;
+
+        // 
+        public DepartamentoController(DbcrudBlazorContext dbcrudBlazor)
+        {
+            _dbContext = dbcrudBlazor;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var responseApi = new ResponseAPI<List<DepartamentoDTO>>();
+
+            var listaDepartamentoDTO = new List<DepartamentoDTO>();
+
+
+            try
+            {
+                foreach(var item in await _dbContext.Departamentos.ToListAsync())
+                {
+                    listaDepartamentoDTO.Add(new DepartamentoDTO
+                    {
+                        IdDepartamento = item.IdDepartamento,
+                        Nombre = item.Nombre,
+                    });
+                }
+
+                responseApi.EsCorrecto = true;
+                responseApi.Valor = listaDepartamentoDTO;
+
+            }
+            catch (Exception ex)
+            {
+                responseApi.EsCorrecto = false;
+                responseApi.Mensaje = ex.Message;
+            }
+
+            // Retornamos...
+            return Ok(responseApi);
+        }
     }
 }
