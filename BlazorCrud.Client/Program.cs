d4423bbc982677d41ad0ffd682df21c49dfe0e9c
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2023-03-07 09:02:38 -0500

    integrando servicios del proyecto

--- BlazorCrud.Client/Program.cs
+++ BlazorCrud.Client/Program.cs
@@ -2,11 +2,16 @@ using BlazorCrud.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using BlazorCrud.Client.Services;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5284") });
+
+builder.Services.AddScoped<IDepartamentoService, DepartamentoService>();
+builder.Services.AddScoped<IEmpleadoService, EmpleadoService>();
 
 await builder.Build().RunAsync();
